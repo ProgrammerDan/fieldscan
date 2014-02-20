e46f896a611071c9e4f7c11dacf868dd9594d901
ProgrammerDan <programmerdan@gmail.com>
2014-02-20 14:48:18 +0000

    master: First commit from mobile. Dao start.

---
+++ src/main/java/com/programmerdan/fieldscan/dao/BaseDao.java
@@ -0,0 +1,20 @@
+package com.programmerdan.fieldscan.dao;
+
+/**
+ * Simple DAO pattern to keep my data access organized.
+ * Probably overkill for this project, so I might strip
+ * it back out. We will see!
+ *
+ * @author Daniel Boston <programmerdan@gmail.com>
+ * @since 0.1-SNAPSHOT
+ * @version 0.1-SNAPSHOT
+ *   Basic CRUD and find implementation.
+ */
+public interface BaseDao<T,K> {
+	
+	public T find(K key);
+	public boolean save(T obj);
+	public Set<T> findAll();
+	public boolean delete(T obj);
+	public T create();
+}

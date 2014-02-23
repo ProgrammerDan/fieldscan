773a624009a06568d69031d4743b79de62237a95
Daniel Boston <programmerdan@gmail.com>
2014-02-23 13:01:58 -0500

    master: Tightening up the Dao Factory, adding in comments and imports.

--- src/main/java/com/programmerdan/fieldscan/dao/FieldScanDaoFactory.java
+++ src/main/java/com/programmerdan/fieldscan/dao/FieldScanDaoFactory.java
@@ -1,5 +1,23 @@
 package com.programmerdan.fieldscan.dao;
 
+import com.programmerdan.fieldscan.dao.impl.FieldScanConfigDaoImpl;
+import com.programmerdan.fieldscan.dao.impl.NodeProcessorConfigDaoImpl;
+import com.programmerdan.fieldscan.dao.impl.FileNodeDaoImpl;
+import com.programmerdan.fieldscan.dao.impl.DirNodeDaoImpl;
+
+import com.programmerdan.fieldscan.dao.FieldScanConfigDao;
+import com.programmerdan.fieldscan.dao.NodeProcessorConfigDao;
+import com.programmerdan.fieldscan.dao.FileNodeDao;
+import com.programmerdan.fieldscan.dao.DirNodeDao;
+import com.programmerdan.fieldscan.dao.BaseDao;
+
+import com.programmerdan.fieldscan.dao.DaoNotAvailableException;
+
+import com.programmerdan.fieldscan.model.FieldScanConfig;
+import com.programmerdan.fieldscan.model.NodeProcessorConfig;
+import com.programmerdan.fieldscan.model.FileNode;
+import com.programmerdan.fieldscan.model.DirNode;
+
 /**
  * Trying something a little different for me and adding another level of
  * abstraction to future proof my design. Instead of dealing with DAO
@@ -21,6 +39,9 @@ public class FieldScanDaoFactory {
 	
 	private static FieldScanDaoFactory instance = null;
 
+	/**
+	 * Singleton pattern, protected so subclasses can mess with it.
+	 */
 	protected FieldScanDaoFactory() {
 		configDao = null;
 		processorDao = null;
@@ -28,6 +49,11 @@ public class FieldScanDaoFactory {
 		dirDao = null;
 	}
 
+	/**
+	 * Get or create the singleton instance of this FieldScanDaoFactory.
+	 *
+	 * @return the FieldScanDaoFactory instance
+	 */
 	public FieldScanDaoFactory getInstance() {
 		if (instance == null) {
 			instance = new FieldScanDaoFactory();
@@ -36,20 +62,32 @@ public class FieldScanDaoFactory {
 		return instance;
 	}
 
+	/**
+	 * Get a DAO based on an object. This could be useful for NodeProcessor
+	 * subclasses if they need access to FileNodes or something similar.
+	 *
+	 * @param daoType the Class type of the object to get a DAO for.
+	 * @return the DAO implementation as the generic {@link BaseDao}.
+	 */
 	public <T> BaseDao<T, ?> getDaoFor(Class<T> daoType) {
 		if (daoType.equals(FieldScanConfig.class) ) {
 			return getFieldScanConfigDao();
-		} else if (daoType.equals(NodeProcessorConfigDao.class) ) {
+		} else if (daoType.equals(NodeProcessorConfig.class) ) {
 			return getNodeProcessorConfigDao();
 		} else if (daoType.equals(FileNode.class) ) {
 			return getFileNodeDao();
 		} else if (daoType.equals(DirNode.class) ) {
 			return getDirNodeDao();
 		} else {
-			throw new DaoNotAvailable(daoType);
+			throw new DaoNotAvailableException(daoType);
 		}
 	}
 
+	/**
+	 * Gets the FieldScanConfigDao using the standard implementation.
+	 *
+	 * @return the {@link FieldScanConfigDao} instance.
+	 */
 	public FieldScanConfigDao getFieldScanConfigDao() {
 		if (configDao == null) {
 			configDao = new FieldScanConfigDaoImpl();
@@ -57,6 +95,11 @@ public class FieldScanDaoFactory {
 		return configDao;
 	}
 
+	/**
+	 * Gets the NodeProcessorConfigDao using the standard implementation.
+	 *
+	 * @return the {@link NodeProcessorConfigDao} instance.
+	 */
 	public NodeProcessorConfigDao getNodeProcessorConfigDao() {
 		if (processorDao == null) {
 			processorDao = new NodeProcessorConfigDaoImpl();
@@ -64,6 +107,11 @@ public class FieldScanDaoFactory {
 		return processorDao;
 	}
 
+	/**
+	 * Gets the FileNodeDao using the standard implementation.
+	 *
+	 * @return the {@link FileNodeDao} instance.
+	 */
 	public FileNodeDao getFileNodeDao() {
 		if (fileDao == null) {
 			fileDao = new FileNodeDaoImpl();
@@ -71,6 +119,11 @@ public class FieldScanDaoFactory {
 		return fileDao;
 	}
 
+	/**
+	 * Gets the DirNodeDao using the standard implementation.
+	 *
+	 * @return the {@link DirNodeDao} instance.
+	 */
 	public DirNodeDao getDirNodeDao() {
 		if (dirDao == null) {
 			dirDao = new DirNodeDaoImpl();

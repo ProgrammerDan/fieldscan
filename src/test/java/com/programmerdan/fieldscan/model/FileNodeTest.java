b1c26b2e3b7aa50d5a998e8772ce30f0558d7349
ProgrammerDan <programmerdan@gmail.com>
2014-02-27 15:23:31 +0000

    master: adding some negative test cases to ensure constraints are enforced.

--- src/test/java/com/programmerdan/fieldscan/model/FileNodeTest.java
+++ src/test/java/com/programmerdan/fieldscan/model/FileNodeTest.java
@@ -35,4 +35,82 @@ public class FileNodeTest extends BaseTest {
 	}
 
 	// TODO: negative tests for null constraints
+	
+	/**
+	 * Testing for expected error state of no DirNode. TODO: narrow exception expectation.
+	 */
+	@Test(expects=PersistenceException)
+	public void fileNodeFailsOnPersistWithoutDirNode {
+		FileNode file2 = new FileNode()
+		em().persist(file2);
+		file2.setFileName("file2");
+		file2.setOneKbHash(new Byte[]{2});
+		file2.setFullHash(new Byte[]{3});
+		file2.setFileSize(101L);
+		file2.setIsGone(true);
+		file2.setDirNode(null);
+
+		em.flush();
+	}
+
+	/**
+	 * Testing for expected error state of no FileName. TODO: narrow exception expectation.
+	 */
+	@Test(expects=PersistenceException)
+	public void fileNodeFailsOnPersistWithoutFileName {
+		DirNode dn = new DirNode(1L,"root3", null, null);
+		em().persist(dn);
+
+		FileNode file2 = new FileNode()
+		em().persist(file2);
+		file2.setFileName(null);
+		file2.setOneKbHash(new Byte[]{2});
+		file2.setFullHash(new Byte[]{3});
+		file2.setFileSize(101L);
+		file2.setIsGone(true);
+		file2.setDirNode(dn);
+
+		em.flush();
+	}
+
+	/**
+	 * Testing for expected error state of no OneKbHash. TODO: narrow exception expectation.
+	 */
+	@Test(expects=PersistenceException)
+	public void fileNodeFailsOnPersistWithoutOneKbHash {
+		DirNode dn = new DirNode(1L,"root4", null, null);
+		em().persist(dn);
+
+		FileNode file2 = new FileNode()
+		em().persist(file2);
+		file2.setFileName("file3");
+		file2.setOneKbHash(null);
+		file2.setFullHash(new Byte[]{3});
+		file2.setFileSize(101L);
+		file2.setIsGone(true);
+		file2.setDirNode(dn);
+
+		em.flush();
+	}
+
+	/**
+	 * Testing for expected error state of no FullHash. TODO: narrow exception expectation.
+	 */
+	@Test(expects=PersistenceException)
+	public void fileNodeFailsOnPersistWithoutFullHash {
+		DirNode dn = new DirNode(1L,"root5", null, null);
+		em().persist(dn);
+
+		FileNode file2 = new FileNode()
+		em().persist(file2);
+		file2.setFileName("file4");
+		file2.setOneKbHash(new Byte[]{4});
+		file2.setFullHash(null);
+		file2.setFileSize(101L);
+		file2.setIsGone(true);
+		file2.setDirNode(dn);
+
+		em.flush();
+	}
+
 }
